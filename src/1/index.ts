3f7d599f003d641ad4df23186b1518650d285b3a
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2022-12-01 08:27:17 +0100

    day1

--- src/1/index.ts
+++ src/1/index.ts
@@ -1,8 +1,41 @@
 import { readAs } from "aoc-util";
 
-const input = readAs<number[]>({
-  parser: (input: string[]) => input.map((i) => parseInt(i)),
+const splitArray = (input: number[]) => {
+  const result = [];
+  let temp = [];
+  for (let i = 0; i <= input.length; i++) {
+    if (isNaN(input[i]) || i > input.length - 1) {
+      result.push(temp);
+      temp = [];
+    } else {
+      temp.push(input[i]);
+    }
+  }
+  return result;
+};
+
+const input = readAs<number[][]>({
+  parser: (input: string[]) => splitArray(input.map((i) => parseInt(i))),
   path: "./src/1/input.txt",
+  splitter: /\n/,
 });
 
-console.log(input);
+const getSums = (input: number[][]) => {
+  return input.map((i) => i.reduce((a, b) => a + b, 0));
+};
+
+const solvePart1 = (input: number[][]) => {
+  console.log(Math.max(...getSums(input)));
+};
+
+const solvePart2 = (input: number[][]) => {
+  const top3 = getSums(input)
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, b) => a + b, 0);
+
+  console.log(top3);
+};
+
+solvePart1(input);
+solvePart2(input);
